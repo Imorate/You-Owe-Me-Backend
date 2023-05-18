0d2d596d5710b12cba6f18158056b638a0eee18d
Amir Mohammad Hl <dev.imorate@gmail.com>
2023-05-19 02:22:38 +0330

    Initial commit

---
+++ src/main/java/ir/imorate/yom/YouOweMeApplication.java
@@ -0,0 +1,13 @@
+package ir.imorate.yom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouOweMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouOweMeApplication.class, args);
+    }
+
+}
