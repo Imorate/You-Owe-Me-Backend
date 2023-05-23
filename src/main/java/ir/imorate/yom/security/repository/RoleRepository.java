0e12f0ad9eb0ee863c8409c77926563cb24a3ef9
Amir Mohammad Hl <dev.imorate@gmail.com>
2023-05-23 21:15:09 +0330

    Update User and Role repositories

--- src/main/java/ir/imorate/yom/security/repository/RoleRepository.java
+++ src/main/java/ir/imorate/yom/security/repository/RoleRepository.java
@@ -4,9 +4,11 @@ import ir.imorate.yom.security.entity.Role;
 import ir.imorate.yom.security.entity.RoleType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 
     Optional<Role> findByType(@NonNull RoleType type);
--- src/main/java/ir/imorate/yom/security/repository/UserRepository.java
+++ src/main/java/ir/imorate/yom/security/repository/UserRepository.java
@@ -3,9 +3,11 @@ package ir.imorate.yom.security.repository;
 import ir.imorate.yom.security.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsernameIgnoreCase(@NonNull String username);
