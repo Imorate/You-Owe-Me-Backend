c0cd488369238d84210e9920701d40caa2abd369
Amir Mohammad Hl <dev.imorate@gmail.com>
2023-05-28 23:28:35 +0330

    Add BCryptEncoder bean

--- src/main/java/ir/imorate/yom/configuration/ApplicationConfiguration.java
+++ src/main/java/ir/imorate/yom/configuration/ApplicationConfiguration.java
@@ -3,9 +3,15 @@ package ir.imorate.yom.configuration;
 import net.datafaker.Faker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class ApplicationConfiguration {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public Faker faker() {
