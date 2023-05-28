947c4db11415722cab636d919e2b3642419dc563
Amir Mohammad Hl <dev.imorate@gmail.com>
2023-05-29 00:38:54 +0330

    Add initial data and update properties

---
+++ src/main/resources/sql/data-role.sql
@@ -0,0 +1,20 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0 */;
+/*!40101 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping data for table yom.role: ~1 rows (approximately)
+DELETE
+FROM `role`;
+/*!40000 ALTER TABLE `role`
+    DISABLE KEYS */;
+INSERT INTO `role` (`id`, `description`, `type`)
+VALUES (1, 'Normal user role', 'USER'),
+       (2, 'Admin user role', 'ADMIN');
+/*!40000 ALTER TABLE `role`
+    ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE = IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS = IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
