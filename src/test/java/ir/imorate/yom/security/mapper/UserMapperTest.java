7c55492a2ce4f562c4d14278028c75985dbde09b
Amir Mohammad Hl <dev.imorate@gmail.com>
2023-06-02 03:18:01 +0330

    Update UserMapper and remove Faker bean

--- src/test/java/ir/imorate/yom/security/mapper/UserMapperTest.java
+++ src/test/java/ir/imorate/yom/security/mapper/UserMapperTest.java
@@ -2,7 +2,6 @@ package ir.imorate.yom.security.mapper;
 
 import ir.imorate.yom.security.entity.User;
 import ir.imorate.yom.security.request.CreateUserRequest;
-import net.datafaker.Faker;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,7 +13,7 @@ import org.springframework.test.context.ContextConfiguration;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@ContextConfiguration(classes = {UserMapperImpl.class, Faker.class})
+@ContextConfiguration(classes = UserMapperImpl.class)
 @ActiveProfiles("test")
 @DisplayName("User mapper test")
 class UserMapperTest {
@@ -22,54 +21,46 @@ class UserMapperTest {
     @Autowired
     private UserMapper userMapper;
 
-    @Autowired
-    private Faker faker;
-
-    private String username;
-
-    private String password;
-
-    private String email;
+    private User user;
 
     @BeforeEach
     void setUp() {
-        username = faker.name().username();
-        email = faker.internet().emailAddress();
-        password = faker.internet().password(8, 256, true, true, true);
+        user = User.builder()
+                .username("test")
+                .email("test@gmail.com")
+                .password("123456")
+                .build();
     }
 
     @Test
-    @DisplayName("toUser() test")
+    @DisplayName("Mapper to user entity")
     void toUser() {
         CreateUserRequest request = null;
 
         assertThat(userMapper.toUser(request)).isNull();
 
-        request = new CreateUserRequest(username, password, email);
+        request = new CreateUserRequest(user.getUsername(), user.getPassword(), user.getEmail());
         User user = userMapper.toUser(request);
 
-        assertThat(user.getUsername()).isEqualTo(username);
-        assertThat(user.getPassword()).isEqualTo(password);
-        assertThat(user.getEmail()).isEqualTo(email);
+        assertFields(user.getUsername(), user.getPassword(), user.getEmail());
     }
 
     @Test
-    @DisplayName("toCreateUserRequest() test")
+    @DisplayName("Mapper to request")
     void toCreateUserRequest() {
-        User user = null;
+        User nullUser = null;
 
-        assertThat(userMapper.toCreateUserRequest(user)).isNull();
+        assertThat(userMapper.toCreateUserRequest(nullUser)).isNull();
 
-        user = User.builder()
-                .username(username)
-                .password(password)
-                .email(email)
-                .build();
         CreateUserRequest request = userMapper.toCreateUserRequest(user);
 
-        assertThat(request.username()).isEqualTo(username);
-        assertThat(request.password()).isEqualTo(password);
-        assertThat(request.email()).isEqualTo(email);
+        assertFields(request.username(), request.password(), request.email());
+    }
+
+    private void assertFields(String username, String password, String email) {
+        assertThat(username).isEqualTo(user.getUsername());
+        assertThat(password).isEqualTo(user.getPassword());
+        assertThat(email).isEqualTo(user.getEmail());
     }
 
 }
